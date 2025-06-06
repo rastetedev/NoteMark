150c43a2a5331a5b7d5a8132eb90dd78e7670bf5
Raul Emilio Astete Acuña <raulastete.2215@gmail.com>
2025-06-06 10:01:22 -0500

    Set up authorization repository

---
+++ app/src/main/java/com/raulastete/notemark/data/repository/DefaultAuthorizationRepository.kt
@@ -0,0 +1,58 @@
+package com.raulastete.notemark.data.repository
+
+import com.raulastete.notemark.data.remote.dto.authentication.AuthorizationResponse
+import com.raulastete.notemark.data.remote.dto.authentication.LoginRequest
+import com.raulastete.notemark.data.remote.dto.authentication.RegistrationRequest
+import com.raulastete.notemark.data.remote.service.ktor.ApiUrl
+import com.raulastete.notemark.data.remote.service.ktor.post
+import com.raulastete.notemark.domain.DataError
+import com.raulastete.notemark.domain.EmptyDataResult
+import com.raulastete.notemark.domain.Result
+import com.raulastete.notemark.domain.SessionStorage
+import com.raulastete.notemark.domain.asEmptyDataResult
+import com.raulastete.notemark.domain.entity.AuthInfo
+import com.raulastete.notemark.domain.repository.AuthorizationRepository
+import io.ktor.client.HttpClient
+
+class DefaultAuthorizationRepository(
+    private val httpClient: HttpClient,
+    private val sessionStorage: SessionStorage
+) : AuthorizationRepository {
+
+    override suspend fun login(
+        email: String,
+        password: String
+    ): EmptyDataResult<DataError.Network> {
+        val result = httpClient.post<LoginRequest, AuthorizationResponse>(
+            route = ApiUrl.LOGIN,
+            body = LoginRequest(
+                email = email,
+                password = password
+            )
+        )
+        if (result is Result.Success) {
+            sessionStorage.set(
+                AuthInfo(
+                    accessToken = result.data.accessToken,
+                    refreshToken = result.data.refreshToken
+                )
+            )
+        }
+        return result.asEmptyDataResult()
+    }
+
+    override suspend fun registration(
+        username: String,
+        email: String,
+        password: String
+    ): EmptyDataResult<DataError.Network> {
+        return httpClient.post<RegistrationRequest, Unit>(
+            route = ApiUrl.REGISTRATION,
+            body = RegistrationRequest(
+                username = username,
+                email = email,
+                password = password
+            )
+        )
+    }
+}
---
+++ app/src/main/java/com/raulastete/notemark/domain/repository/AuthorizationRepository.kt
@@ -0,0 +1,18 @@
+package com.raulastete.notemark.domain.repository
+
+import com.raulastete.notemark.domain.DataError
+import com.raulastete.notemark.domain.EmptyDataResult
+
+interface AuthorizationRepository {
+
+    suspend fun login(
+        email: String,
+        password: String
+    ): EmptyDataResult<DataError.Network>
+
+    suspend fun registration(
+        username: String,
+        email: String,
+        password: String
+    ): EmptyDataResult<DataError.Network>
+}
